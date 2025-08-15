ca3b19a385f36b61d587844ab5061ef36b36063a
Ayan Kumar Patra <ayankumarpatra51@gmail.com>
2025-08-15 14:20:07 +0530

    Added npr also

--- 01_functions/Q01_ncr_npr.cpp
+++ 01_functions/Q01_ncr_npr.cpp
@@ -1,39 +1,39 @@
 #include<iostream>
 using namespace std ;
-    int factorialvalue (int n){
+    int factorial_value (int n){
         if (n<0){
-            cout<<"Negetive numbers cant have factorial value.Error! ";
+            cout<<"Negetive numbers cant have factorial value.Error!\n";
             return 0;
         }
         if (n==0 or n==1)
             return 1;
-        return n*factorialvalue(n-1);
+        return n*factorial_value(n-1);
                 }
 
     int ncr_value (int n , int r){
         if (n>0 && r>=0 && n>=r) {
-            return factorialvalue(n) /(factorialvalue(r) *factorialvalue(n-r));
+            return factorial_value(n) /(factorial_value(r) *factorial_value(n-r));
         } 
-        else cout<<"Sorry ! cant claculate ncr value for the given values \n";
-        return -1;
+        else cout<<"Sorry ! cant calculate ncr value for the given values \n";
+        return 0;
+    }
+
+    int npr_value(int n , int r ){
+    if (n>0 && r>=0 && n>=r) 
+        return factorial_value(n)*factorial_value(n-r);
+    else { cout<<"Sorry ! cant calculate npr value for the given values \n";
+        return 0;
+    }
     }
 int main (){
-/*
-git add .
-git commit -m "Added Functions folder with initial codes"
-git push
 
-ncr = n! /r! * n-r!
-*/
 int num ,r;
 cout<<"Enter the total number of items (n): ";
 cin>>num;
 cout<<"Enter the number of items to choose (r): ";
 cin>>r;
-//cout<<"Factorial value is : "<<factorialvalue(num);
-cout<<"ncr value is : "<<ncr_value(num,r);
-
-//cout<<"Factorial value is : "<<factorialvalue(num);
-
+//cout<<"Factorial value is : "<<factorial_value(num);
+cout<<"ncr value is : "<<ncr_value(num,r)<<endl;
+cout<<"npr value is : "<<npr_value(num,r)<<endl;
     return 0;
 }
