24aeceface8a1265b65a5fac5bfa23bfcd0aa64e
Ayan Kumar Patra <ayankumarpatra51@gmail.com>
2025-08-16 22:30:01 +0530

    Added Hcf finder for both loop and eukledian approach

--- 01_functions/Q02_pascals_traingle.cpp
+++ 01_functions/Q02_pascals_traingle.cpp
@@ -82,5 +82,43 @@ int main() {
 }
 
 
+or shortly optimized as 
 
+
+// base code without spaces ;
+#include<iostream>
+using namespace std ;
+int main (){
+    int n;
+    cout<<"Enter no of lines : ";
+    cin>>n;
+    for (int i=0;i<n;i++){
+        int curr =1;
+        for (int j=0;j<=i;j++){
+            cout<<curr<<" ";
+            curr=(i-j)/(j+1);
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+added spaces
 */
+#include<iostream>
+using namespace std ;
+int main (){
+    int n;
+    cout<<"Enter no of lines : ";
+    cin>>n;
+    for (int i=0;i<n;i++){
+        int curr =1,spaces=n-i;
+        while(spaces-->0)
+            cout<<" ";
+        for (int j=0;j<=i;j++){
+            cout<<curr<<" ";
+            curr=curr*(i-j)/(j+1);
+        }
+        cout<<endl;
+    }
+    return 0;
+}
---
+++ 01_functions/Q03_Hcf_of2num.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std ;
+int gcd_finder(int a, int b){
+    if (a==0 || b==0){
+        cout<<"Invalid numbers to calculate GCD"<<endl;
+        return -1;
+    }
+    if (a<0) a*=-1;
+    if (b<0) b*=-1;
+    int big= (a>=b)? a:b ,gcd=1;
+    for (int i=1;i<big;i++){
+        if (a%i==0 && b%i==0)
+            gcd=i;
+    }
+    /*
+    or Eukledian approach for less time complexity ..
+        while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+        return a;
+    */
+    return gcd;
+}
+int main (){
+int num1,num2;
+cout<<"Enter the numbers ";
+cin>>num1>>num2;
+cout<<"GCD of the numbers is "<<gcd_finder(num1,num2);
+    return 0;
+}
