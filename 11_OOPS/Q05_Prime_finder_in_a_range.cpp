04221378fccdca10639cf0b78de0ca2a9cf2654d
Ayan Kumar Patra <ayankumarpatra51@gmail.com>
2025-08-20 09:12:23 +0530

    Added 2 better approach to return number of primes in a range (Sieve of Eratosthenes)

--- 11_OOPS/Q05_Prime_finder_in_a_range.cpp
+++ 11_OOPS/Q05_Prime_finder_in_a_range.cpp
@@ -1,5 +1,6 @@
 // Given an integer n, return the number of prime numbers that are strictly less than n.
 
+/*
 #include<iostream>
 #include<cmath>
 using namespace std ;
@@ -15,14 +16,15 @@ bool prime_checker(int num){
     return true;
 }
 
-int NumberOfPrimes(int range){
-    if(range<=2) return 0;
-    int z=range/2,find=1;
-    for (int i=1;i<z;i++){
-        if (prime_checker(2*i+1))
-            find++;
+int countPrimes(int range) {
+    if (range <= 2) return 0;
+
+    int count = 1;  // count prime "2"
+    for (int i = 3; i < range; i += 2) {  // check only odds
+        if (prime_checker(i))
+            count++;
     }
-    return find;
+    return count;
 }
 
 int main (){
@@ -30,6 +32,73 @@ int range;
 cout<<"Enter the range ";
 cin>>range;
 
-cout<<NumberOfPrimes(range);
+cout<<countPrimes(range);
     return 0;
 }
+
+
+
+sieve of eratos thenesis
+
+
+*/
+
+#include<iostream>
+#include<cmath>
+#include<vector>// to use vector .. , dynamic araay 
+using namespace std ;
+
+class Solution {
+public:
+    void fillSieve (vector<bool>&sieve){
+        int n= sieve.size()-1; 
+        for (int i=2;i<=sqrt(n);i++){
+            for(int j=i*2;j<=n;j+=i){
+                sieve[j]=0;
+            }
+        }
+    };
+    int distinctPrimeFactors(int n) {
+        if (n<=2) return 0;
+        n=n-1;
+        int count =0;
+        vector<bool> sieve(n+1,1);//1 means prime
+        fillSieve(sieve);
+        sieve[0]=0;
+        sieve[1]=0;
+    for (int i=2;i<=n;i++){
+        if (sieve[i]==1) count++;
+    }
+    return count;
+    }
+};
+
+int main (){
+
+    return 0;
+}
+/*
+more better approach 
+
+int countPrimes(int n) {
+    if (n <= 2) return 0;
+
+    vector<bool> isPrime(n, true);  // create array
+    isPrime[0] = isPrime[1] = false;
+
+    for (int i = 2; i * i < n; i++) {
+        if (isPrime[i]) {
+            for (int j = i * i; j < n; j += i) {
+                isPrime[j] = false;  // mark multiples as not prime
+            }
+        }
+    }
+
+    int count = 0;
+    for (int i = 2; i < n; i++) {
+        if (isPrime[i]) count++;
+    }
+    return count;
+}
+
+*/
