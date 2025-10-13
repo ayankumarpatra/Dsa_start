4b45377f1459e75fb6643cd327e58cd6f1c072e2
Ayan Kumar Patra <ayankumarpatra51@gmail.com>
2025-10-13 23:45:27 +0530

    Learned about spiraal filling by linked list

--- 13_Stacks/Q10_(239. Sliding Window Maximum Optimized).cpp
+++ 13_Stacks/Q10_(239. Sliding Window Maximum Optimized).cpp
@@ -11,11 +11,12 @@ Output: [3,3,5,5,6,7]
 
 */
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        //Bruteforce solution
+        //optimized solution
         //Next greater element array
         int i,Vector_size=nums.size();
         stack <int> st;
         vector <int> Next_Greater_element(Vector_size,-1);
+        vector <int> Max_Greater_in_window;
         for(i=Vector_size-1;i>=0;i--){
             while (st.size()>0 && nums[st.top()]<=nums[i])
             {
@@ -26,7 +27,21 @@ Output: [3,3,5,5,6,7]
             }
 
             st.push(i);
+
         }
+        
+        for(i=0;i<=Vector_size-k;i++){
+            
+        int j=i;
+        while (Next_Greater_element[j]!=-1 && Next_Greater_element[j]<i+k){
+            j=Next_Greater_element[j];
+
+        }
+        Max_Greater_in_window.push_back(nums[j]);
+        
+        }
+
+        return Max_Greater_in_window;
     }
 
 int main (){
