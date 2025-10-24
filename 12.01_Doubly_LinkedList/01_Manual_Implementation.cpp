2df42eedafcb42f2ec073e9811c4b82c82754af2
Ayan Kumar Patra <ayankumarpatra51@gmail.com>
2025-10-24 23:56:43 +0530

    Learned about DLL

--- 12.01_Doubly_LinkedList/01_Manual_Implementation.cpp
+++ 12.01_Doubly_LinkedList/01_Manual_Implementation.cpp
@@ -1,11 +1,50 @@
 #include<iostream>
 using namespace std;
 
+/*
+benefits 
+
+1. display linked list in reverse order 
+
+
+remember old pattern display by recursion 
+
+display (head){
+if(head==null){
+return;
+}
+
+cout<<head->val; // print in normal order 
+display(head->next);
+
+// but if done
+disply(head->next);// print in reverse order 
+cout<<head->val;
+
+}
+
+
+2. if any node given , we can reach entire list 
+    single link list given , only forward possible 
+
+
+
+
+
+creating functions to 
+1. insert (head , tail , index )
+2. delete (head , tail , index )
+3. get value (head , tail , index )
+*/
+
+
 class Node{// for doubly linked list
     public:
     Node* prev;
+    Node* head;
     int val;
     Node* next;
+    Node * tail;
 
     Node(int value){
         prev=NULL;
@@ -14,14 +53,16 @@ class Node{// for doubly linked list
     }
 };
 
-class LinkedList{
+class DoublyLinkedList{
     public:
     Node* prev;
+    Node* head;
     int val;
     Node* next;
+    Node* tail;
     int size;
 
-    LinkedList(int value){
+    DoublyLinkedList(int value){
         prev=NULL;
         val=value;
         next=NULL;
@@ -30,7 +71,8 @@ class LinkedList{
 
     void AddAthead(int val ){
         if(size==0){
-            
+            Node * temp= new Node(val);
+            head
         }
     }
 };
