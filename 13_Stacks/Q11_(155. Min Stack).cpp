cfc6ee44fd8cd1d014a3c45dd6a67a459d2c41d5
Ayan Kumar Patra <ayankumarpatra51@gmail.com>
2025-11-06 22:56:00 +0530

    Done a prob by vector , tommorow will do it by stack

--- 13_Stacks/Q11_(155. Min Stack).cpp
+++ 13_Stacks/Q11_(155. Min Stack).cpp
@@ -1,27 +1,36 @@
 #include<iostream>
 #include<stack>
+#include<vector>
+#include<climits>
 using namespace std ;
 
 class MinStack {
 public:
-    MinStack() {
-        stack <int> st;
-    }
+    stack<int> st;
+    vector<int> minv;
+
+    MinStack() { }// minstack constructor
     
     void push(int val) {
-        
+        if(minv.empty() || val<=minv.back()){
+            minv.push_back(val);
+        }
+        st.push(val);
     }
     
     void pop() {
-        
+        if(minv.size()>0 && st.top()==minv[minv.size()-1]){//minv.back() same
+            minv.pop_back();
+        }
+        st.pop();
     }
     
     int top() {
-        
+        return st.top();
     }
     
     int getMin() {
-        
+        return minv.back();
     }
 };
 
