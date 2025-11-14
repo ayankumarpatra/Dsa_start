dc6cce55d06c394d075bcb9ef5532d46b1ffd0ad
Ayan Kumar Patra <ayankumarpatra51@gmail.com>
2025-11-14 22:48:05 +0530

    Learned linked list dqueue

--- 14_Queue/03_LinkedList_Representation.cpp
+++ 14_Queue/03_LinkedList_Representation.cpp
@@ -2,6 +2,7 @@
 #include<stack>
 #include<vector>
 
+using namespace std;
 /*
 push back 
 pop front 
@@ -30,13 +31,13 @@ class node {
 };
 
 
-class LinkedList{
+class LinkedList_Queue{
     public:
     node* head;
     node* tail;
     int size;
 
-    LinkedList(){
+    LinkedList_Queue(){
         head=tail=NULL;
         size=0;
     }
@@ -45,10 +46,12 @@ class LinkedList{
         node* temp=new node(num);
         if (size==0){
             head=tail=temp;
+            size++;
         }
         else {
             tail->next=temp;
             tail=temp;
+            size++;
         }
     }
 
@@ -61,24 +64,84 @@ class LinkedList{
             node* temp=head->next;
             delete(head);
             head=temp;
+            size--;
         }
     }
 
-    int top(){
+    int rear(){
         if (size==0){
             cout<<"Empty Queue no top element \n";
-            return;
+            return -1;
         }
         else {
             return tail->val;
         }
     }
 
+    int front(){
+        if (size==0){
+            cout<<"Empty Queue ,no element \n";
+            return -1;
+        }
+        else {
+            return head->val;
+        }
+    }
+
+    void Display(){
+        if (size==0){
+            cout<<"Empty Queue ,no element \n";
+            return;
+        }
+        else {
+            node* temp=head;
+            while (temp!=NULL)
+            {
+                cout<<temp->val<<" ";
+                temp=temp->next;
+            }
+            
+        }    
+        
+    }
 };
 
-using namespace std ;
 int main (){
+LinkedList_Queue lq;
+int choice;
+do {
+    cout<<"\nAvailable options are :\n1.push 2.pop\n3.get rear element 4.get front element\n5.display 6.Any other key to exit\nEnter your choice:";
+    cin>>choice;
 
+    switch (choice)
+    {
+        case 1:int element;
+                cout<<"Enter Element to push : ";
+                cin>>element;
+                lq.push(element);
+                break;
+        case 2: lq.pop();
+                break;
+        case 3: cout<<"Rear element is "<<lq.rear()<<endl;
+                break;
+        case 4 : cout<<"Front element is "<<lq.front()<<endl;
+                break;
+        case 5 : lq.Display();
+                break;
+        default: cout<<"exitting program\n";
+                return 0;
+    }
+
+}while(choice!=0);
     
     return 0;
 }
+
+/*
+Advantage of linked list implementation of queue over array implementation
+1. unlimited size
+2. no wastage of size 
+3. normal not complex implementation like a normal linked list 
+
+
+*/
---
+++ 14_Queue/04_Deque.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+using namespace std ;
+
+/*
+
+deque is a doubly ended queue , means here like normal queue 
+
+push back , pop front possible and also 
+
+push front pop back possible and both at o(1) time complexity 
+
+
+usually implemented by array or doubly linked list 
+*/
+class node {
+    
+};
+
+
+
+int main (){
+
+    
+    return 0;
+}
