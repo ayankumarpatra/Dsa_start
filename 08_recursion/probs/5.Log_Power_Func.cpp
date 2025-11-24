a0ad32f3056a407a4004508b8a7456ee3e1d7d2a
Ayan Kumar Patra <ayankumarpatra51@gmail.com>
2025-11-24 20:09:17 +0530

    revisited recursin

--- 08_recursion/probs/5.Log_Power_Func.cpp
+++ 08_recursion/probs/5.Log_Power_Func.cpp
@@ -1,14 +1,46 @@
+// calculate a^b and return value 
+
 #include<iostream>
-#include<stack>
-#include<vector>
 
 using namespace std ;
 
 
+int calculate_Power(int num,int power){
 
+    if(power==0){
+        return 1;
+    }
+
+    else if(power==1){
+        return num;
+    }
+
+    int ans = calculate_Power(num,power/2);
+
+    if(power%2==0){
+
+        return ans*ans;
+    }
+
+    else if(power%2!=0){
+        return num*ans*ans;
+    }
+
+    return 0;
+}
 
 int main (){
 
+    int num,power;
+
+    cout<<"Enter NUmber : ";
+    cin>>num;
     
+    cout<<"Warning ! This version only works for positive and non negetive integer power \n";
+    cout<<"Enter Power accordingly : ";
+    cin>>power;
+
+    cout<<num<<"^"<<power<<" = "<<calculate_Power(num,power);
+
     return 0;
 }
