51c817a8df1b8b373afe0c0136732e58bdc027ec
Ayan Kumar Patra <ayankumarpatra51@gmail.com>
2025-12-06 21:59:15 +0530

    revisited recursion and vector storing subset

--- 08_recursion/leetcode/01_(78. Subsets).cpp
+++ 08_recursion/leetcode/01_(78. Subsets).cpp
@@ -1,14 +1,31 @@
 #include<iostream>
-#include<stack>
 #include<vector>
 
 using namespace std ;
 
 class Solution {
 public:
-    void SubsetAdder(vector<vector<int>> subsets)
+    
+    void Subset_generator(vector<int>& mainset,vector<int>& helper, vector<vector<int>>& ans,int i){
+        if(i==mainset.size()){
+            ans.push_back(helper);
+            return;
+        }
+        // include the current element
+
+        helper.push_back(mainset[i]);
+        Subset_generator(mainset,helper,ans,i+1);
+
+        // removing the elemnt 
+        helper.pop_back();
+        Subset_generator(mainset,helper,ans,i+1);
+    }
+
     vector<vector<int>> subsets(vector<int>& nums) {
-        
+        vector<vector<int>> ans;
+        vector<int> helper;
+        Subset_generator(nums,helper,ans,0);
+        return ans;
     }
 };
 
