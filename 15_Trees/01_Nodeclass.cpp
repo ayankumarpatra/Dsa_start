5ee668c4a57f1414be438f63f50627aeffe04078
Ayan Kumar Patra <ayankumarpatra51@gmail.com>
2025-12-28 15:56:52 +0530

    solved dpp

---
+++ 15_Trees/01_Nodeclass.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+
+using namespace std ;
+
+class node {
+    public :
+    int val;
+    node* left;
+    node* right;
+
+    node(int val){
+        this->val=val;
+        left=right=NULL;
+    }
+};
+
+
+int main (){
+
+    
+    return 0;
+}
---
+++ DPP/16.(2483. Minimum Penalty for a Shop).cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+#include<climits>
+
+using namespace std ;
+
+class Solution {
+public:
+    int bestClosingTime(string customers) {
+
+        int n=customers.size(),current_penalty=0;
+        
+
+    }
+};
+
+
+int main (){
+
+    
+    return 0;
+}
