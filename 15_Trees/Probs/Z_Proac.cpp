465ade1b675cfa268270e3e0bc957589b01057ae
Ayan Kumar Patra <ayankumarpatra51@gmail.com>
2025-12-30 12:21:19 +0530

    Starting leetcode

---
+++ 15_Trees/Probs/Z_Proac.cpp
@@ -0,0 +1,54 @@
+// find level of a ctree
+
+#include<iostream>
+
+using namespace std ;
+
+class Node {
+    public :
+    int val;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        this->val=val;
+        left=right=NULL;
+    }
+};
+
+int Tree_Level_Finder(Node * root){
+
+    if (root==NULL){
+        return 0;
+    }
+
+    return (1+(Tree_Level_Finder(root->left)+Tree_Level_Finder(root->right)));
+
+}
+
+
+int main (){
+
+    Node* a=new Node(1);
+    Node* b=new Node(2);
+    Node* c=new Node(3);
+    Node* d=new Node(4);
+    Node* e=new Node(5);
+    Node* f=new Node(6);
+    Node* g=new Node(7);
+    Node* h=new Node(7);
+
+    a->left=b;
+    a->right=c;
+
+    b->left=d;
+    b->right=e;
+
+    c->left=f;
+    f->right=g;
+    g->left=h;
+
+// Height = level-1
+    cout<<"Tree Level is "<<Tree_Level_Finder(a);
+    return 0;
+}
