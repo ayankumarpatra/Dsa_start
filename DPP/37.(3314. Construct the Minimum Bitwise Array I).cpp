cd3f540af205a32a95e1bae14bb543f3f946bf8a
Ayan Kumar Patra <ayankumarpatra51@gmail.com>
2026-01-21 10:52:27 +0530

    Done dpp 20 1

---
+++ DPP/37.(3314. Construct the Minimum Bitwise Array I).cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+
+using namespace std ;
+
+class Solution {
+public:
+    vector<int> minBitwiseArray(vector<int>& nums) {
+        vector<int> ans;
+        for (int num : nums) {
+
+            int found = -1;       
+            for (int x = 0; x < num; x++) {
+                if ((x | (x + 1)) == num) {
+                    found = x; 
+                    break; 
+                }
+            }       
+          ans.push_back(found); 
+        }
+        
+        return ans;
+    }
+};
+
+
+
+int main (){
+
+    
+    return 0;
+}
