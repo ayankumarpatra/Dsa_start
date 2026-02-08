2570a9a45cebece7296c7da25469d0f446b7ea5e
Ayan Kumar Patra <ayankumarpatra51@gmail.com>
2026-02-08 08:45:58 +0530

    done leetcode dpp 7 2

---
+++ DPP/53.(1653. Minimum Deletions to Make String Balanced).cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+
+using namespace std ;
+
+
+class Solution {
+public:
+    int minimumDeletions(string s) {
+        int bCount = 0;
+        int deletions = 0;
+
+        for(char c : s) {
+            if(c == 'b') {
+                bCount++;
+            }
+            else {
+                deletions = min(deletions + 1, bCount);
+            }
+        }
+        return deletions;
+    }
+};
+
+
+int main (){
+
+    
+    return 0;
+}
