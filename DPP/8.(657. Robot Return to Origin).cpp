6068bc53fa7a90ac2896d10eb3064f3ee3f09e9f
Ayan Kumar Patra <ayankumarpatra51@gmail.com>
2026-04-05 23:00:08 +0530

    done leetcode dpp 4 4

---
+++ DPP/8.(657. Robot Return to Origin).cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+
+using namespace std ;
+
+class Solution {
+public:
+    bool judgeCircle(string& moves) {
+        int H=0, V=0;
+        for(char c: moves){
+            H+=(c=='R');
+            H-=(c=='L');
+            V+=(c=='U');
+            V-=(c=='D');
+        }
+        return (H==0) & (V==0);
+    }
+};
+
+
+int main (){
+
+    
+    return 0;
+}
