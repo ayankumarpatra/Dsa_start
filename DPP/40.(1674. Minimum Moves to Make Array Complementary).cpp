06b175291b58d063d2b9b9de748266a34fecf8b7
Ayan Kumar Patra <ayankumarpatra51@gmail.com>
2026-05-13 16:11:16 +0530

    done dpp

---
+++ DPP/40.(1674. Minimum Moves to Make Array Complementary).cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+
+using namespace std ;
+
+class Solution {
+public:
+    int minMoves(vector<int>& nums, int limit) {
+        
+    }
+};
+
+
+int main (){
+
+    
+    return 0;
+}
