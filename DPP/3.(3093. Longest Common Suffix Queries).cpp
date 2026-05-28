34eb7aa597b8897196c059e1fef1a69cf1344f79
Ayan Kumar Patra <ayankumarpatra51@gmail.com>
2026-05-28 19:08:22 +0530

    revisited some old concepts

---
+++ DPP/3.(3093. Longest Common Suffix Queries).cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<stack>
+#include<queue>
+#include<vector>
+#include<algorithm>
+
+using namespace std ;
+
+
+class Solution {
+public:
+    vector<int> stringIndices(vector<string>& wordsContainer, vector<string>& wordsQuery) {
+        
+    }
+};
+
+
+int main (){
+
+    
+    return 0;
+}
