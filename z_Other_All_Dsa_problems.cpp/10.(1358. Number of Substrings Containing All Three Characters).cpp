80184765cd805b67b6b74cd6cb44ea74255eca27
Ayan Kumar Patra <ayankumarpatra51@gmail.com>
2026-06-30 14:08:51 +0530

    done dpp

---
+++ z_Other_All_Dsa_problems.cpp/10.(1358. Number of Substrings Containing All Three Characters).cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<stack>
+#include<queue>
+#include<vector>
+#include<algorithm>
+
+using namespace std ;
+
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        int lastA = -1, lastB = -1, lastC = -1;
+        int total = 0;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == 'a')
+                lastA = i;
+            else if (s[i] == 'b')
+                lastB = i;
+            else
+                lastC = i;
+
+            if (lastA != -1 && lastB != -1 && lastC != -1) {
+                total += min({lastA, lastB, lastC}) + 1;
+            }
+        }
+
+        return total;
+    }
+};
+
+
+int main (){
+
+    
+    return 0;
+}
